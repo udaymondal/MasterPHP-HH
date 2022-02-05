20f42a448964d4c4a72ea1172a5faa4a063277e9
udaymondal <udaysk.mondal@gmail.com>
2022-02-05 22:02:16 +0600

    Section1

---
+++ Section 1/php01.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hello World!!!";
+echo "\n"; // new line \n does not work in a single quote.
+echo "\n"; 
+//variables 
+
+$myName = "Lucifer";
+$age = 30;
+$boyosh = "age";
+
+echo "The name's {$myName} and living for {$age} years";
+echo "\n";
+echo "The name's {$myName} and living for {$$boyosh} years"; // double dollar signs can be used to make another variable inside a string.
